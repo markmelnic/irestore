ffcb0d8df45612d05b4e0f0708600d6e0afc0fa4
Mark Melnic <markhappy200@gmail.com>
2021-03-11 11:53:40 +0200

    Specified tokens length

--- api/models.py
+++ api/models.py
@@ -3,8 +3,8 @@ from api import db
 class Tokens(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    access_token = db.Column(db.String, default="")
-    refresh_token = db.Column(db.String, default="")
+    access_token = db.Column(db.String(255), default="")
+    refresh_token = db.Column(db.String(255), default="")
 
 class TelegramUsers(db.Model):
     id = db.Column(db.Integer, primary_key=True)

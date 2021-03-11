a28b0599565e3540ba9b39d3ba40cb53021d3155
Mark Melnic <markhappy200@gmail.com>
2021-03-11 18:52:12 +0200

    Changed tokens type to text

--- api/models.py
+++ api/models.py
@@ -3,8 +3,8 @@ from api import db
 class Tokens(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    access_token = db.Column(db.String(255), default="")
-    refresh_token = db.Column(db.String(255), default="")
+    access_token = db.Column(db.Text, default="")
+    refresh_token = db.Column(db.Text, default="")
 
 class TelegramUsers(db.Model):
     id = db.Column(db.Integer, primary_key=True)

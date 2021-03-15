7196cc1da4a0d2970246233948c36d61bdbccaad
Mark Melnic <markhappy200@gmail.com>
2021-03-15 23:19:59 +0200

    Secured secret as envar

--- api/routes.py
+++ api/routes.py
@@ -5,10 +5,8 @@ from flask import request, redirect, url_for
 from api import *
 from .models import Tokens
 
-SECRET = 'Vadim'
-
 def verify_webhook(data, hmac_header):
-    digest = hmac.new(SECRET, data.encode('utf-8'), hashlib.sha256).digest()
+    digest = hmac.new(getenv('SPF_SECRET'), data.encode('utf-8'), hashlib.sha256).digest()
     computed_hmac = base64.b64encode(digest)
     return hmac.compare_digest(computed_hmac, hmac_header.encode('utf-8'))
 

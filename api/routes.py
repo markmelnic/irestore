9002a2e012a0c134e8992f67a775d2ee2041342e
Mark Melnic <markhappy200@gmail.com>
2021-03-17 14:46:51 +0200

    [Test]

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,5 @@
-import requests, json, hmac, hashlib, base64
+import requests, json, hmac, hashlib, base64, random
+import urllib.parse as urlparse
 from os import getenv
 from flask import request, redirect, url_for
 
@@ -61,13 +62,78 @@ def init():
 def api_redirect():
     return redirect('https://www.amocrm.ru/oauth?client_id='+getenv('CLIENT_ID')+'&state='+getenv('REDIRECT')+'&mode=post_message')
 
+@app.route('/api/service', methods=['POST', 'GET'])
+def api_service():
+    encodedStr = b'http://foo.appspot.com/?leads%5Bstatus%5D%5B0%5D%5Bid%5D=30267969&leads%5Bstatus%5D%5B0%5D%5Bname%5D=apple+tv+3&leads%5Bstatus%5D%5B0%5D%5Bstatus_id%5D=16525558&leads%5Bstatus%5D%5B0%5D%5Bold_status_id%5D=16525555&leads%5Bstatus%5D%5B0%5D%5Bprice%5D=1&leads%5Bstatus%5D%5B0%5D%5Bresponsible_user_id%5D=1764433&leads%5Bstatus%5D%5B0%5D%5Blast_modified%5D=1615985846&leads%5Bstatus%5D%5B0%5D%5Bmodified_user_id%5D=1760815&leads%5Bstatus%5D%5B0%5D%5Bcreated_user_id%5D=1764433&leads%5Bstatus%5D%5B0%5D%5Bdate_create%5D=1615894221&leads%5Bstatus%5D%5B0%5D%5Bpipeline_id%5D=788512&leads%5Bstatus%5D%5B0%5D%5Btags%5D%5B0%5D%5Bid%5D=482205&leads%5Bstatus%5D%5B0%5D%5Btags%5D%5B0%5D%5Bname%5D=Sasa&leads%5Bstatus%5D%5B0%5D%5Baccount_id%5D=16503244&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B0%5D%5Bid%5D=491053&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B0%5D%5Bname%5D=IMEI%2FSERIE&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B0%5D%5Bvalues%5D%5B0%5D%5Bvalue%5D=1&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B1%5D%5Bid%5D=481305&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B1%5D%5Bname%5D=Cind+trebuie%3F&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B1%5D%5Bvalues%5D%5B0%5D=1615845600&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B2%5D%5Bid%5D=496779&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B2%5D%5Bname%5D=Stare+Device&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B2%5D%5Bvalues%5D%5B0%5D%5Bvalue%5D=Zghirieturi+minore&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B2%5D%5Bvalues%5D%5B0%5D%5Benum%5D=959761&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B3%5D%5Bid%5D=481313&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B3%5D%5Bname%5D=Tip+Lead&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B3%5D%5Bvalues%5D%5B0%5D%5Bvalue%5D=Lead+nou&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B3%5D%5Bvalues%5D%5B0%5D%5Benum%5D=932119&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B4%5D%5Bid%5D=481307&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B4%5D%5Bname%5D=Unde+au+aflat%3F&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B4%5D%5Bvalues%5D%5B0%5D%5Bvalue%5D=Google&leads%5Bstatus%5D%5B0%5D%5Bcustom_fields%5D%5B4%5D%5Bvalues%5D%5B0%5D%5Benum%5D=932073&leads%5Bstatus%5D%5B0%5D%5Bcreated_at%5D=1615894221&leads%5Bstatus%5D%5B0%5D%5Bupdated_at%5D=1615985846&account%5Bsubdomain%5D=irestoremoldova&account%5Bid%5D=16503244&account%5B_links%5D%5Bself%5D=https%3A%2F%2Firestoremoldova.amocrm.ru'
+
+    url = 'http://foo.appspot.com/abc?def=ghi'
+    parsed = urlparse.urlparse(encodedStr.decode("utf-8"))
+    #print(parse_qs(parsed.query))
+    id = urlparse.parse_qs(parsed.query)['leads[status][0][id]'][0]
+    print(AMO.get_leads_links(id))
+
+
+    leads = request.args.get('leads')
+    print(leads)
+    if leads:
+        for l in leads:
+            print(l)
+            if int(l['status_id']) == 16525558:
+                print(l)
+                return l
+            else:
+                return "X"
+    else:
+        return "A"
+
+@app.route('/api/sync_products', methods=['POST', 'GET'])
+def sync_products():
+    temp = [[str(v['sku']) +'|'+ str(v['product_id']) for v in p['variants']] for p in SPF.get_products()['products']]
+    products = []
+    c = 0
+    for __ in temp:
+        for _ in __:
+            c += 1
+            products.append({"value": _, "sort": c})
+
+    return AMO.get_lead(30269101)
+
+    custom = [
+        {
+            "name": "TEST BLEA",
+            "type": "multiselect",
+            "enums": products,
+            "is_deletable": False,
+            "is_visible": True,
+            "is_required": True,
+            "required_statuses": [{
+                "pipeline_id": PIPELINE
+            }]
+        }
+    ]
+    #print(custom)
+    #AMO.create_leads_custom_fields(custom)
+
+    return pos()
+
 @app.route('/api/add_custom_fields')
 def add_custom_fields():
     fs = ["name", "phone", "email", "address", "product", "sku"]
-    fields = []
+    fields = {}
+    fields['add'] = []
     for f in fs:
-        fields.append({"type": "text", "name": f})
-    print(fields)
+        origin_id = random.randint(1111,9999)
+        fields['add'].append({
+            "name": f,
+            "field_type": "1",
+            "element_type": "2",
+            #"origin": f"{origin_id}_irestore",
+            "is_editable": "0",
+        })
+    #print(fields)
+
+    for f in AMO.get_leads_custom_fields()['_embedded']['custom_fields']:
+        print(f)
     AMO.create_leads_custom_fields(fs)
 
     return pos()

06e8a892b7640e7f37ceb6ae5921c3930d4ce591
Mark Melnic <markhappy200@gmail.com>
2021-04-13 17:09:02 +0300

    t

--- api/routes.py
+++ api/routes.py
@@ -76,24 +76,19 @@ def init():
 def api_redirect():
     return redirect('https://www.amocrm.ru/oauth?client_id='+getenv('CLIENT_ID')+'&state='+getenv('REDIRECT')+'&mode=post_message')
 
-SERVICE = {}
-SERVICE2 = {}
+S = {}
 @app.route('/api/service', methods=['POST', 'GET'])
 def service():
-    global SERVICE
-    SERVICE = request.get_data()
-    print(SERVICE)
-    SERVICE2 = json.loads(request.get_data())
-    print(SERVICE2)
+    global S
+    data = request.get_data()
+    data = urlparse.unquote(data.decode('utf8'))
+    print(data)
+    S = data
     return {'data': request.get_data()}
 
 @app.route('/api/service_test', methods=['POST', 'GET'])
-def service_test():
-    return SERVICE
-@app.route('/api/service_test2', methods=['POST', 'GET'])
-def service_test2():
-    return SERVICE2
-
+def service():
+    return S
 
 @app.route('/api/add_custom_fields')
 def add_custom_fields():
